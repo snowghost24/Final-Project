04f79f0a26c20d40abce4577fd3feedf4b1515ac
Snowghost24 <Snowghost@Joses-MacBook-Air.local>
2017-10-15 13:27:21 -0400

    added database connection

--- config/sqlconnection.js
+++ config/sqlconnection.js
@@ -1,26 +1,26 @@
-// // Set up MySQL connection.
-// var mysql = require("mysql");
-// var connection;
-// //TODO:set jaws db environement variable 
-// if(process.env.JAWSDB_URL) {
-//   //Heroku deployment
-//     connection = mysql.createConnection(process.env.JAWSDB_URL);
-// } else {
-//   connection = mysql.createConnection({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   database : process.env.DB_NAME,
-//   });
-// }
-// // Make connection.
-// connection.connect(function (err) {
-//   if (err) {
-//     console.error("error connecting: " + err.stack);
-//     return;
-//   }
-//   console.log("connected as id " + connection.threadId);
-// });
+// Set up MySQL connection.
+var mysql = require("mysql");
+var connection;
+//TODO:set jaws db environement variable 
+if(process.env.JAWSDB_URL) {
+  //Heroku deployment
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database : process.env.DB_NAME,
+  });
+}
+// Make connection.
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
 
-// // Export connection for our ORM to use.
-// module.exports = connection;
+// Export connection for our ORM to use.
+module.exports = connection;
 
