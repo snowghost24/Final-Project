2d553c4db7d5606549746849104381a69343cc0e
Snowghost24 <Snowghost@Joses-MacBook-Air.local>
2017-10-15 13:30:53 -0400

    added db to storage session

--- app.js
+++ app.js
@@ -15,33 +15,33 @@ var routes = require("./routes/controller.js")
 app.use("/", routes);
 
 
-// required for passport
-// var options;
-// if (process.env.JAWSDB_URL) {
-//     //Heroku deployment
-//     options = {
-//       host: process.env.PRODUCTION_DB_HOST,
-//       user: process.env.PRODUCTION_DB_USER,
-//       password: process.env.PRODUCTION_DB_PASSWORD,
-//       database: process.env.PRODUCTION_DB_NAME,
-//       port:PORT
-//     };
-// } else {
-//     options = {
-//         host: process.env.DB_HOST,
-//         user: process.env.DB_USER,
-//         password: process.env.DB_PASSWORD,
-//         database: process.env.DB_NAME
-//     };
-// }
+//required for passport
+var options;
+if (process.env.JAWSDB_URL) {
+    //Heroku deployment
+    options = {
+      host: process.env.PRODUCTION_DB_HOST,
+      user: process.env.PRODUCTION_DB_USER,
+      password: process.env.PRODUCTION_DB_PASSWORD,
+      database: process.env.PRODUCTION_DB_NAME,
+      port:PORT
+    };
+} else {
+    options = {
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
+    };
+}
 
-// var sessionStore = new MySQLStore(options);
-// app.use(session({
-//     secret: 'aritzahadababyitzagirl', // session secret
-//     store: sessionStore,
-//     resave: true,
-//     saveUninitialized: false
-// }));
+var sessionStore = new MySQLStore(options);
+app.use(session({
+    secret: 'aritzahadababyitzagirl', // session secret
+    store: sessionStore,
+    resave: true,
+    saveUninitialized: false
+}));
 
 
 app.listen(PORT,() => {
