94c9a6b8c41b0867e038ef38cb2018bfba563545
Snowghost24 <Snowghost@Joses-MacBook-Air.local>
2017-10-21 18:44:06 -0400

    handlebars wont show results

--- routes/controller.js
+++ routes/controller.js
@@ -1,26 +1,13 @@
 var express = require('express');
 var router = express.Router();
-
 var parseString = require('xml2js').parseString;
 var axios = require('axios');
-
 var street = encodeURIComponent(" 2514 hiker court");
 var city = encodeURIComponent("kissimmee FL");
 var count = 2;
 var zpid = 48749425;
-
-
-
-
-
-
-
-
-
-
-
-
-
+var lastSold = "";
+var pass1;
 
 
 router.get('/',(req,res)=>{
@@ -45,7 +32,7 @@ router.get('/',(req,res)=>{
               parseString(theData, {trim: true}, function (err, result) {
                   results = result['Comps:comps'].response[0].properties[0].comparables[0]
                   for (var i = 0; i < count; i++) {
-                    console.log(results.comp[i]. yearBuilt)        
+                    console.log(results.comp[i].yearBuilt)        
                   } 
             }); 
             })
@@ -55,29 +42,19 @@ router.get('/',(req,res)=>{
       }
    res.render('index.handlebars')
 })
+
+
+
+
+
+
 // ────────────────────────────────────────────────────────────────────────────────
 
 router.post('/made',(req,res)=>{
-
   console.log(req.body.responseData[0])
   var street = encodeURIComponent(req.body.responseData[0]);
   var city = encodeURIComponent(req.body.responseData[1]+" "+req.body.responseData[2]);
-  var lastSold = "";
-  // axios.post(`http://www.zillow.com/webservice/GetSearchResults.htm?zws-id=X1-ZWz18yof9snguj_4eq90&address=${street}&citystatezip=${city}`)
-  // .then(function (response) {
-  // //   console.log(response.data);
-  //   var theData = response.data;
-  //   parseString(theData, {trim: true}, function (err, result) {
-  //         // var theZpid = result['SearchResults:searchresults'].response[0].results[0].result[0].zpid[0];
-  //         var soldDate = result['SearchResults:searchresults'].response[0].results[0].result[0].zestimate[0]
-  //         console.log(soldDate);
-  // });
-  // })
-  // .catch(function (error) {
-  //   console.log(error);
-  // });
-
-
+  
   axios.post(`http://www.zillow.com/webservice/GetSearchResults.htm?zws-id=X1-ZWz18yof9snguj_4eq90&address=${street}&citystatezip=${city}`)
   .then(function (response) {
   //   console.log(response.data);
@@ -100,6 +77,9 @@ router.post('/made',(req,res)=>{
       parseString(theData, {trim: true}, function (err, result) {  
                 lastSold = result["SearchResults:searchresults"].response[0].results[0].result[0].lastSoldDate[0];
         console.log(lastSold);
+        // console.log(res);
+        res.render('index',{hbData:lastSold})
+        // pass1(lastSold);
        }); 
         })
         .catch(function (error) {
@@ -112,10 +92,5 @@ router.post('/made',(req,res)=>{
    });
 // ────────────────────────────────────────────────────────────────────────────────
 
-  //  router.get('/house',(req,res)=>{
-  //     parseString(req.body, function (err, result) {
-  //        console.dir(req.body);
-  //     });
-  //  })
 
 module.exports = router;
